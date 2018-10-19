7cf02c063dfd51546cda8253932a0f8479c8bbe1
DmitriChe <juneority@mail.ru>
2018-10-19 11:36:06 +0300

    First commit

---
+++ mainapp/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render
+
+def main(request):
+    return render(request, 'mainapp/index.html')
+
+def products(request):
+    return render(request, 'mainapp/pages/catalog.html')
+
+def contacts(request):
+    return render(request, 'mainapp/pages/contacts.html')
+
+def bicycle(request):
+    return render(request, 'mainapp/pages/bicycle.html')
+
+def car(request):
+    return render(request, 'mainapp/pages/car.html')
+
+def helicopter(request):
+    return render(request, 'mainapp/pages/helicopter.html')
+    
+def airplane(request):
+    return render(request, 'mainapp/pages/airplane.html')
