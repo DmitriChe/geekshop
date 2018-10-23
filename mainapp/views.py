8353987300df25bd22d1f7c251cfedcd28d9090e
DmitriChe <juneority@mail.ru>
2018-10-23 16:35:42 +0300

    ввел контекстные переменные для динамическое генерации списка товаров в каталоге

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.template import Template
 from django.http import HttpResponse
-from django.template.loader import get_template
+from django.template.loader import get_template, render_to_string
 
 # Create your views here.
 from django.shortcuts import render
@@ -10,7 +10,18 @@ def main(request):
     return render(request, 'mainapp/index.html')
 
 def products(request):
-    return render(request, 'mainapp/pages/catalog.html')
+    # return render(request, 'mainapp/pages/catalog.html')
+    context = {
+        'pagename' : 'каталог товаров',
+        'products' : [
+            ['bicycle', '/static/images/bicycle.jpg','велосипед'],
+            ['car', '/static/images/car.jpg', 'автомобиль'],
+            ['helicopter', '/static/images/helicopter.jpg', 'вертолёт'],
+            ['airplane', '/static/images/airplane.jpg', 'самолёт'],
+        ]
+    }
+    response_string = render_to_string('mainapp/pages/catalog.html', context)
+    return HttpResponse(response_string)
 
 def contacts(request):
     return render(request, 'mainapp/pages/contacts.html')
