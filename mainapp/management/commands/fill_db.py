f6560baa32642ecb13fdebb590459016cc7066e4
DmitriChe <juneority@mail.ru>
2018-10-30 14:35:48 +0300

    еще какие-то файлы появились...

--- mainapp/management/commands/fill_db.py
+++ mainapp/management/commands/fill_db.py
@@ -30,5 +30,6 @@ class Command(BaseCommand):
             product['category'] = _category
             new_product = Product(**product)
             new_product.save()
+
         # Создаем суперпользователя при помощи менеджера модели
         super_user = User.objects.create_superuser('django', 'django@geekshop.local', 'geekbrains')
