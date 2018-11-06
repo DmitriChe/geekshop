b935ef4068e28ef9731f054754979793ac116f51
DmitriChe <juneority@mail.ru>
2018-11-06 14:24:23 +0300

    Исправил генерацию ссылки на категорию каталога! Спасибо, Ольга!

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
 import mainapp.views as mainapp
 
-app_name = 'catalog'
+# app_name = 'catalog'
 
 urlpatterns = [
     url(r'^$', mainapp.products_list, name='index'),
